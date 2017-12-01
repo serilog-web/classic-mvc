f99b98f639c6569004bf1485a46d8907ed14a25c
Thibaud DESODT <thibaud.desodt@dgtresor.gouv.fr>
2017-12-01 14:01:27 +0100

    Initial repo layout and solution

---
+++ src/SerilogWeb.Classic.Mvc/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerilogWeb.Classic.Mvc
+{
+    public class Class1
+    {
+    }
+}
---
+++ test/SerilogWeb.Classic.Mvc.Test/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SerilogWeb.Classic.Mvc.Test.MvcApplication" Language="C#" %>
