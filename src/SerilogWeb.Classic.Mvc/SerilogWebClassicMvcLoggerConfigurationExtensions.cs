ece87338e63a68ca3ac35c978c1999dd92c07026
Daniel García Rubio <dgarciarubio.Software@gmail.com>
2018-05-29 11:03:34 +0200

    Use only one extension method per enricher, with optional parameters

--- src/SerilogWeb.Classic.Mvc/SerilogWebClassicMvcLoggerConfigurationExtensions.cs
+++ src/SerilogWeb.Classic.Mvc/SerilogWebClassicMvcLoggerConfigurationExtensions.cs
@@ -17,23 +17,11 @@ namespace Serilog
         /// Enrich log events with the name of the current MVC action.
         /// </summary>
         /// <param name="enrichmentConfiguration">Logger enrichment configuration.</param>
-        /// <returns>Configuration object allowing method chaining.</returns>
-        public static LoggerConfiguration WithMvcActionName(
-            this LoggerEnrichmentConfiguration enrichmentConfiguration)
-        {
-            if (enrichmentConfiguration == null) throw new ArgumentNullException(nameof(enrichmentConfiguration));
-            return enrichmentConfiguration.With<MvcActionNameEnricher>();
-        }
-
-        /// <summary>
-        /// Enrich log events with the name of the current MVC action.
-        /// </summary>
-        /// <param name="enrichmentConfiguration">Logger enrichment configuration.</param>
         /// <param name="propertyName">Name of the property to log.</param>
         /// <returns>Configuration object allowing method chaining.</returns>
         public static LoggerConfiguration WithMvcActionName(
             this LoggerEnrichmentConfiguration enrichmentConfiguration,
-            string propertyName)
+            string propertyName = MvcActionNameEnricher.MvcActionPropertyName)
         {
             if (enrichmentConfiguration == null) throw new ArgumentNullException(nameof(enrichmentConfiguration));
             return enrichmentConfiguration.With(new MvcActionNameEnricher(propertyName));
@@ -43,23 +31,11 @@ namespace Serilog
         /// Enrich log events with the controller name for the current MVC action.
         /// </summary>
         /// <param name="enrichmentConfiguration">Logger enrichment configuration.</param>
-        /// <returns>Configuration object allowing method chaining.</returns>
-        public static LoggerConfiguration WithMvcControllerName(
-            this LoggerEnrichmentConfiguration enrichmentConfiguration)
-        {
-            if (enrichmentConfiguration == null) throw new ArgumentNullException(nameof(enrichmentConfiguration));
-            return enrichmentConfiguration.With<MvcControllerNameEnricher>();
-        }
-
-        /// <summary>
-        /// Enrich log events with the controller name for the current MVC action.
-        /// </summary>
-        /// <param name="enrichmentConfiguration">Logger enrichment configuration.</param>
         /// <param name="propertyName">Name of the property to log.</param>
         /// <returns>Configuration object allowing method chaining.</returns>
         public static LoggerConfiguration WithMvcControllerName(
             this LoggerEnrichmentConfiguration enrichmentConfiguration,
-            string propertyName)
+            string propertyName = MvcControllerNameEnricher.MvcControllerPropertyName)
         {
             if (enrichmentConfiguration == null) throw new ArgumentNullException(nameof(enrichmentConfiguration));
             return enrichmentConfiguration.With(new MvcControllerNameEnricher(propertyName));
@@ -69,38 +45,14 @@ namespace Serilog
         /// Enrich log events with the route data for the current MVC action.
         /// </summary>
         /// <param name="enrichmentConfiguration">Logger enrichment configuration.</param>
-        /// <returns>Configuration object allowing method chaining.</returns>
-        public static LoggerConfiguration WithMvcRouteData(
-            this LoggerEnrichmentConfiguration enrichmentConfiguration)
-        {
-            if (enrichmentConfiguration == null) throw new ArgumentNullException(nameof(enrichmentConfiguration));
-            return enrichmentConfiguration.With<MvcRouteDataEnricher>();
-        }
-
-        /// <summary>
-        /// Enrich log events with the route data for the current MVC action.
-        /// </summary>
-        /// <param name="enrichmentConfiguration">Logger enrichment configuration.</param>
         /// <param name="propertyName">Name of the property to log.</param>
         /// <returns>Configuration object allowing method chaining.</returns>
         public static LoggerConfiguration WithMvcRouteData(
             this LoggerEnrichmentConfiguration enrichmentConfiguration,
-            string propertyName)
+            string propertyName = MvcRouteDataEnricher.MvcRouteDataPropertyName)
         {
             if (enrichmentConfiguration == null) throw new ArgumentNullException(nameof(enrichmentConfiguration));
             return enrichmentConfiguration.With(new MvcRouteDataEnricher(propertyName));
-        }
-
-        /// <summary>
-        /// Enrich log events with the route template for the current MVC action.
-        /// </summary>
-        /// <param name="enrichmentConfiguration">Logger enrichment configuration.</param>
-        /// <returns>Configuration object allowing method chaining.</returns>
-        public static LoggerConfiguration WithMvcRouteTemplate(
-            this LoggerEnrichmentConfiguration enrichmentConfiguration)
-        {
-            if (enrichmentConfiguration == null) throw new ArgumentNullException(nameof(enrichmentConfiguration));
-            return enrichmentConfiguration.With<MvcRouteTemplateEnricher>();
         }
 
         /// <summary>
@@ -111,7 +63,7 @@ namespace Serilog
         /// <returns>Configuration object allowing method chaining.</returns>
         public static LoggerConfiguration WithMvcRouteTemplate(
             this LoggerEnrichmentConfiguration enrichmentConfiguration,
-            string propertyName)
+            string propertyName = MvcRouteTemplateEnricher.MvcRouteTemplatePropertyName)
         {
             if (enrichmentConfiguration == null) throw new ArgumentNullException(nameof(enrichmentConfiguration));
             return enrichmentConfiguration.With(new MvcRouteTemplateEnricher(propertyName));
